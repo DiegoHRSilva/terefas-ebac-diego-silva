6516c665a6c20f0c58a0e5a3520aff4c6e71b019
DiegoHRSilva <106542160+DiegoHRSilva@users.noreply.github.com>
2023-06-07 17:35:03 -0300

    exercicio do modulo 8

---
+++ TarefaCalculoMedia/src/ExercicioModulo8.java
@@ -0,0 +1,18 @@
+public class ExercicioModulo8 {
+
+    public static void main(String args[]){
+        calculoMedia();
+    }
+
+    public static void calculoMedia() {
+        int num1=2;
+        int num2=4;
+        int num3=6;
+        int num4=8;
+
+        int resultado=(num1+num2+num3+num4)/4;
+        System.out.print("A media dos quatro numeros é: ");
+        System.out.println(resultado);
+
+    }
+}
