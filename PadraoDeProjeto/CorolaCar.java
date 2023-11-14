c27f67d72bf902fb24f938aeef4ccb6edf832566
DiegoHRSilva <106542160+DiegoHRSilva@users.noreply.github.com>
2023-11-14 14:11:54 -0300

    Add files via upload

---
+++ PadraoDeProjeto/CorolaCar.java
@@ -0,0 +1,8 @@
+package carfactory;
+
+public class CorolaCar extends Car{
+
+  public CorolaCar(int horsePower, String fuelSource, String color) {
+      super(horsePower, fuelSource, color);
+  }
+}
