f846ac5ff39f00e185209810ac35716d81ba9fa8
DiegoHRSilva <106542160+DiegoHRSilva@users.noreply.github.com>
2024-03-28 10:11:11 -0300

    Create IProdutoDAO.java
    commit

---
+++ TarefaMod25/src/main/java/br/com/diego/dao/IProdutoDAO.java
@@ -0,0 +1,8 @@
+package br.com.diego.dao;
+
+import br.com.diego.dao.generic.IGenericDAO;
+import br.com.diego.domain.Produto;
+
+public interface IProdutoDAO extends IGenericDAO<Produto, String>{
+
+}
