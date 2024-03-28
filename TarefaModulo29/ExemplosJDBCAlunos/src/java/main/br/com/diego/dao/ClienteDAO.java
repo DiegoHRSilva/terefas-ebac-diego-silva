757fd6427760cdd80378c199450194ce1d21a033
DiegoHRSilva <106542160+DiegoHRSilva@users.noreply.github.com>
2024-03-28 11:51:54 -0300

    Update ClienteDAO.java
    commit

--- TarefaModulo29/ExemplosJDBCAlunos/src/java/main/br/com/diego/dao/ClienteDAO.java
+++ TarefaModulo29/ExemplosJDBCAlunos/src/java/main/br/com/diego/dao/ClienteDAO.java
@@ -31,6 +31,23 @@ public class ClienteDAO implements IClienteDAO {
 			}
 		}
 	}
+	
+	@Override
+	public Integer atualizar(Cliente cliente) throws Exception {
+		Connection connection = null;
+    	PreparedStatement stm = null;
+		try {
+			connection = ConnectionFactory.getConnection();
+			String sql = getSqlUpdate();
+			stm = connection.prepareStatement(sql);
+			adicionarParametrosUpdate(stm, cliente);
+			return stm.executeUpdate();
+		} catch(Exception e) {
+			throw e;
+		} finally {
+			closeConnection(connection, stm, null);
+		}
+	}
 
 	@Override
 	public Cliente consultar(String codigo) throws Exception {
@@ -85,32 +102,33 @@ public class ClienteDAO implements IClienteDAO {
 		}
 	}
 	@Override
-	public Cliente buscarTodos() throws Exception {
+	public List<Cliente> buscarTodos() throws Exception {
 		Connection connection = null;
-		PreparedStatement stm = null;
-		ResultSet rs = null;
-		Cliente cliente = null;
+    	PreparedStatement stm = null;
+    	ResultSet rs = null;
+    	List<Cliente> list = new ArrayList<>();
+    	Cliente cliente = null;
 		try {
 			connection = ConnectionFactory.getConnection();
-			String sql = "select * from tb_cliente_2";
+			String sql = getSqlSelectAll();
 			stm = connection.prepareStatement(sql);
 			rs = stm.executeQuery();
-			if (rs.next()) {
-				cliente = new Cliente();
-				cliente.setId(rs.getLong("id"));
-				cliente.setCodigo(rs.getString("codigo"));
-				cliente.setNome(rs.getString("nome"));
-			}
-			return cliente;
+			
+		    while (rs.next()) {
+		    	cliente = new Cliente();
+		    	Long id = rs.getLong("ID");
+		    	String nome = rs.getString("NOME");
+		    	String cd = rs.getString("CODIGO");
+		    	cliente.setId(id);
+		    	cliente.setNome(nome);
+		    	cliente.setCodigo(cd);
+		    	list.add(cliente);
+		    }
 		} catch(Exception e) {
 			throw e;
 		} finally {
-			if (stm != null && !stm.isClosed()) {
-				stm.close();
-			}
-			if (connection != null && !connection.isClosed()) {
-				connection.close();
-			}
+			closeConnection(connection, stm, rs);
 		}
+		return list;
 	}
 }
